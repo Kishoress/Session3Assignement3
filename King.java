c38676abd129efd05030b78b0022f266bc886a0f
Kishore S S <shrek.kishore@gmail.com>
2016-04-09 01:06:58 +0530

    Added files via upload

---
+++ King.java
@@ -0,0 +1,15 @@
+package randomnumber;
+
+public class King {
+	
+	public static void main(String[] args) {
+		
+		int N = 9;
+        double randomNumber = Math.random();
+        int randomInt = (int)(N * randomNumber);
+ 
+        System.out.println(randomInt);
+
+	}
+
+}
